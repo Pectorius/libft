131cf1bd163d1b33566165e1906cb442c4af9ca5
Pierre Negre <pnegre@c3r5p7.42perpignan.fr>
2023-02-15 11:19:45 +0100

    libft_test

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: pnegre <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 13:25:29 by pnegre            #+#    #+#             */
-/*   Updated: 2023/02/14 15:33:52 by pnegre           ###   ########.fr       */
+/*   Updated: 2023/02/15 11:19:04 by pnegre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
-	prt = malloc(count * size);
+	ptr = malloc(count * size);
 	if (ptr == 0)
 		return (0);
 	ft_bzero(ptr, count * size);
